38cb796925ae849ea21c9233560c12ca9c37f6da
pezhiyi <pengzhiyi9910@gmail.com>
2025-03-23 02:11:56 +0800

    修复导入路径错误

--- src/app/api/urls/mapping/route.js
+++ src/app/api/urls/mapping/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { getAllUrlMappings } from '../../../../utils/urlStorage';
+import { getAllUrlMappings } from '../../../utils/urlStorage';
 
 export async function GET(request) {
   try {
